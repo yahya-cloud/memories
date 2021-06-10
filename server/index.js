4b2f25cc3b7f132b43835b1d49b7916612db4aa9
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-06-10 21:59:12 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -11,11 +11,9 @@ import userRoutes from './routes/users.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
+app.use(express.json())
 
 app.use('/posts', postRoutes)
 app.use('/users', userRoutes)
