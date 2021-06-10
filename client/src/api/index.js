113b6800f40f90199f78f6792c6e0bc68b771043
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-06-10 22:30:48 +0530

    commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000' });
+const API = axios.create({baseURL: 'https://sheltered-bayou-78627.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')){
